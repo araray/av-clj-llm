24f8d9a473486ff1126baabeb04aa1f6004c007e
Araray Velho <araray@gmail.com>
2024-03-07 13:55:14 -0300

    updating some dependencies versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject av-clj-llm "0.1.0-SNAPSHOT"
   :description "A Clojure chatbot using different AI APIs"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]
-                 [cheshire "5.10.0"]]
+                 [cheshire "5.12.0"]]
   :main ^:skip-aot av-clj-llm.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
