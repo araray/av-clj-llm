90c1a3db0a7fab1d4e3c7126171dc2c4768d2af1
Araray Velho <araray@gmail.com>
2024-03-13 21:57:26 -0300

    Update project for enhanced LLM API integration with streaming support
    - Update .gitignore to include .clj-kondo/ and .lsp/ for better developer experience.
    - Increment project version to 0.2.0-RC1 in project.clj, indicating significant updates.
    - Introduce core.async dependency for asynchronous communication with AI APIs.
    - Refactor Anthropic and OpenAI chat API implementations to support streaming responses using core.async channels.
    - Extend ChatAPI protocol with `set-streaming!` method to toggle streaming behavior dynamically.
    - Modify api.core namespace to define a constant for stream end signal.
    - Implement streaming logic in utils namespace with API request handling supporting both immediate and streamed responses.
    - Adjust main application logic to handle streamed chat responses, improving interaction flow.
    - Adapt API request utility function to manage concurrent requests and handle streaming responses properly.
    - Ensure error handling and retry mechanism are in place for API requests, enhancing robustness.
    - Miscellaneous code cleanups and improvements for readability and efficiency.
    
    This update brings significant enhancements to the LLM API integration, introducing streaming support to handle real-time chat interactions more effectively. The changes include both structural adjustments and feature enhancements to accommodate streaming data from AI APIs, improving the overall responsiveness and user experience of the chatbot.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject av-clj-llm "0.1.0-SNAPSHOT"
+(defproject av-clj-llm "0.2.0-RC1"
   :description "A Clojure chatbot using different AI APIs"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]
-                 [cheshire "5.12.0"]]
+                 [cheshire "5.12.0"]
+                 [org.clojure/core.async "1.6.673"]]
   :main ^:skip-aot av-clj-llm.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-             :dev {:dependencies [[org.clojure/test.check "1.1.0"]]})
+  :dev {:dependencies [[org.clojure/test.check "1.1.0"]]})
--- src/av_clj_llm/api/openai.clj
+++ src/av_clj_llm/api/openai.clj
@@ -1,14 +1,14 @@
 (ns av-clj-llm.api.openai
   (:require [av-clj-llm.api.core :refer [ChatAPI get-chat-response]]
             [av-clj-llm.utils :as utils]
-            [clj-http.client :as http]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [clojure.core.async :as async]))
 
-(defrecord OpenAIChat [config messages]
+(defrecord OpenAIChat [config messages streaming?]
   ChatAPI
-  (get-chat-response [_ prompt]
+  (get-chat-response [this prompt ch]
     (println "---\nSending prompt:" prompt "\n")
-    (let [url "https://api.openai.com/v1/chat/completions"
+    (let [url (:model-endpoint config)
           headers {"Content-Type" "application/json"
                    "OpenAI-Organization" (:organization config)
                    "Authorization" (str "Bearer " (:api-key config))}
@@ -16,16 +16,23 @@
                 :messages (conj @messages {:role "user" :content prompt})
                 :max_tokens (:max-tokens config)
                 :temperature (:temperature config)
-                :top_p (:top-p config)}
-          response (utils/api-request url headers body)
-          r (-> response :body (json/decode true) :choices first :message)]
-      (swap! messages conj r)
-      (:content r)))
+                :top_p (:top-p config)
+                :stream @streaming?}
+          response (utils/api-request url headers body @streaming?)]
+      (if @streaming?
+        (async/go
+          (async/>! ch (utils/stream-handler response)))
+        (let [r (-> response :body (json/decode true) :choices first :message)]
+          (swap! messages conj r)
+          (async/put! ch (:content r))))))
 
-  (ask [this prompt]
+  (ask [this prompt ch]
     (swap! messages conj {:role "user" :content prompt})
     (let [paginated-prompts (utils/paginate-prompt prompt (:max-tokens config))]
-      (reduce (fn [responses prompt]
-                (conj responses (get-chat-response this prompt)))
-              []
-              paginated-prompts))))
+      (async/go
+        (doseq [prompt paginated-prompts]
+          (get-chat-response this prompt ch))
+        (async/close! ch))))
+
+  (set-streaming! [this streaming]
+    (->OpenAIChat (:config this) (:messages this) (atom streaming))))
